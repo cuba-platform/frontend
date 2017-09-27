b2a45e53d04212f5cb50519bee3f6e0efe76e089
Vlad Minaev <minaev@haulmont.com>
2017-09-27 12:18:24 +0400

    Entities search
    PWC-8

---
+++ packages/cuba-rest-js/dist-node/util.d.ts
@@ -0,0 +1,2 @@
+export declare function base64encode(str: string): string;
+export declare function encodeGetParams(data: any): string;
---
+++ packages/cuba-rest-js/dist-node/util.js
@@ -0,0 +1,34 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function base64encode(str) {
+    /* tslint:disable:no-string-literal */
+    if (typeof btoa === 'function') {
+        return btoa(str);
+    }
+    else if (global['Buffer']) {
+        return new global['Buffer'](str).toString('base64');
+    }
+    else {
+        throw new Error('Unable to encode to base64');
+    }
+    /* tslint:enable:no-string-literal */
+}
+exports.base64encode = base64encode;
+function encodeGetParams(data) {
+    return Object
+        .keys(data)
+        .map(function (key) {
+        return encodeURIComponent(key) + "=" + (encodeURIComponent(serialize(data[key])));
+    })
+        .join("&");
+}
+exports.encodeGetParams = encodeGetParams;
+function serialize(rawParam) {
+    if (rawParam == null) {
+        return '';
+    }
+    if (typeof rawParam === 'object') {
+        return JSON.stringify(rawParam);
+    }
+    return rawParam;
+}
