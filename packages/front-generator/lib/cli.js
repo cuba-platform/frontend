d12f9e08d1bccb40685e279289bc0f6890411c3d
Vlad Minaev <minaev@haulmont.com>
2018-01-30 20:18:44 +0400

    CLI

--- packages/front-generator/lib/cli.js
+++ packages/front-generator/lib/cli.js
@@ -2,4 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const commander = require("commander");
 const program = commander;
-program.version('0.1.0');
+program
+    .version('0.1.0', '-v, --version')
+    .parse(process.argv);
--- packages/front-generator/src/cli.ts
+++ packages/front-generator/src/cli.ts
@@ -2,4 +2,6 @@ import * as commander from "commander";
 
 const program = commander;
 
-program.version('0.1.0');
+program
+    .version('0.1.0', '-v, --version')
+    .parse(process.argv);
