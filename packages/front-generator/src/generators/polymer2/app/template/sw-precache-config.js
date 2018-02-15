8c4f2afdf145301f45fb6050c419980e078b7772
Vlad Minaev <minaev@haulmont.com>
2018-02-15 17:38:59 +0400

    Polymer2 template files

---
+++ packages/front-generator/src/generators/polymer2/app/template/sw-precache-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  replacePrefix: '/${modulePrefix}${project_polymerClient_suffix}/',
+  staticFileGlobs: [
+    'index.html',
+    'src/**.html',
+    'images/*',
+    'manifest.json',
+    'bower_components/webcomponentsjs/webcomponents-loader.js',
+    'bower_components/fetch/fetch.js'
+  ],
+  navigateFallback: 'index.html'
+};
