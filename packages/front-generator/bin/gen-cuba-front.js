40b7855b9f0d911d61520c5be15e2ce81b1b45a2
Vlad Minaev <minaev@haulmont.com>
2018-04-16 18:26:48 +0400

    Fix error code on unhandled promise rejection

--- packages/front-generator/bin/gen-cuba-front.js
+++ packages/front-generator/bin/gen-cuba-front.js
@@ -6,6 +6,7 @@ process.title = 'gen-cuba-front';
 
 process.on('unhandledRejection', (reason, p) => {
   console.log('Unhandled Rejection ', p, ' reason: ', reason);
+  process.exit(1);
 });
 
 require('../lib/cli');
