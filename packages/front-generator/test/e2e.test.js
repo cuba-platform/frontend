7c9d316be7c70fe3aae6f7eaa9efe408538dd34b
Vlad Minaev <minaev@haulmont.com>
2018-06-25 12:08:12 +0400

    chore: minor test refactoring

--- packages/front-generator/test/e2e.test.js
+++ packages/front-generator/test/e2e.test.js
@@ -8,22 +8,27 @@ const answers = require('./answers');
   await rimraf('.tmp/*');
 
   const generationProcesses = [];
-  generationProcesses.push(runGenerator('polymer2:app', '.tmp/polymer2-app'));
-  generationProcesses.push(runGenerator('polymer2:blank-component', '.tmp/polymer2-app/src/component', answers.blankComponent, '../'));
-  generationProcesses.push(runGenerator('polymer2:entity-management', '.tmp/polymer2-app/src/entity-management', answers.entityManagement, '../'));
-  generationProcesses.push(runGenerator('polymer2:entity-cards', '.tmp/polymer2-app/src/entity-cards', answers.entityCards, '../'));
-  generationProcesses.push(runGenerator('polymer2:entity-list', '.tmp/polymer2-app/src/entity-list', answers.entityList, '../'));
-  generationProcesses.push(runGenerator('polymer2:entity-edit', '.tmp/polymer2-app/src/entity-edit', answers.entityEdit, '../'));
-  generationProcesses.push(runGenerator('polymer2:query-results', '.tmp/polymer2-app/src/query-results', answers.queryResults, '../'));
-  generationProcesses.push(runGenerator('polymer2:service-form', '.tmp/polymer2-app/src/service-form', answers.serviceForm, '../'));
-  generationProcesses.push(runGenerator('polymer2:service-data', '.tmp/polymer2-app/src/service-data', answers.serviceData, '../'));
 
-  generationProcesses.push(runGenerator('polymer2-typescript:app', '.tmp/polymer2ts-app'));
-  generationProcesses.push(runGenerator('polymer2-typescript:blank-component', '.tmp/polymer2ts-app/src/component', answers.blankComponent, '../'));
-  generationProcesses.push(runGenerator('polymer2-typescript:entity-cards', '.tmp/polymer2ts-app/src/entity-cards', answers.entityCards, '../'));
-  generationProcesses.push(runGenerator('polymer2-typescript:entity-list', '.tmp/polymer2ts-app/src/entity-list', answers.entityList, '../'));
-  generationProcesses.push(runGenerator('polymer2-typescript:entity-edit', '.tmp/polymer2ts-app/src/entity-edit', answers.entityEdit, '../'));
-  generationProcesses.push(runGenerator('polymer2-typescript:entity-management', '.tmp/polymer2ts-app/src/entity-management', answers.entityManagement, '../'));
+  const polymer2AppDir = '.tmp/polymer2-app';
+
+  generationProcesses.push(runGenerator('polymer2:app', polymer2AppDir));
+  generationProcesses.push(runGenerator('polymer2:blank-component', `${polymer2AppDir}/src/component`, answers.blankComponent, '../'));
+  generationProcesses.push(runGenerator('polymer2:entity-management', `${polymer2AppDir}/src/entity-management`, answers.entityManagement, '../'));
+  generationProcesses.push(runGenerator('polymer2:entity-cards', `${polymer2AppDir}/src/entity-cards`, answers.entityCards, '../'));
+  generationProcesses.push(runGenerator('polymer2:entity-list', `${polymer2AppDir}/src/entity-list`, answers.entityList, '../'));
+  generationProcesses.push(runGenerator('polymer2:entity-edit', `${polymer2AppDir}/src/entity-edit`, answers.entityEdit, '../'));
+  generationProcesses.push(runGenerator('polymer2:query-results', `${polymer2AppDir}/src/query-results`, answers.queryResults, '../'));
+  generationProcesses.push(runGenerator('polymer2:service-form', `${polymer2AppDir}/src/service-form`, answers.serviceForm, '../'));
+  generationProcesses.push(runGenerator('polymer2:service-data', `${polymer2AppDir}/src/service-data`, answers.serviceData, '../'));
+
+  const polymer2tsAppDir = '.tmp/polymer2-app';
+
+  generationProcesses.push(runGenerator('polymer2-typescript:app', polymer2tsAppDir));
+  generationProcesses.push(runGenerator('polymer2-typescript:blank-component', `${polymer2tsAppDir}/src/component`, answers.blankComponent, '../'));
+  generationProcesses.push(runGenerator('polymer2-typescript:entity-cards', `${polymer2tsAppDir}/src/entity-cards`, answers.entityCards, '../'));
+  generationProcesses.push(runGenerator('polymer2-typescript:entity-list', `${polymer2tsAppDir}/src/entity-list`, answers.entityList, '../'));
+  generationProcesses.push(runGenerator('polymer2-typescript:entity-edit', `${polymer2tsAppDir}/src/entity-edit`, answers.entityEdit, '../'));
+  generationProcesses.push(runGenerator('polymer2-typescript:entity-management', `${polymer2tsAppDir}/src/entity-management`, answers.entityManagement, '../'));
 
   Promise.all(generationProcesses).catch((e) => {
     console.log(e);
