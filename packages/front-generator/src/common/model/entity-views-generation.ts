2b13310069c7704e1c237a41c010c640b535fca8
Vadim Basko <vadim.p.basko@gmail.com>
2019-09-10 17:39:04 +0400

    SDK Views: do not include properties of inheritors #18

--- packages/front-generator/src/common/model/entity-views-generation.ts
+++ packages/front-generator/src/common/model/entity-views-generation.ts
@@ -1,5 +1,7 @@
-import {Entity, ProjectModel, View} from "./cuba-model";
+import {Entity, EntityAttribute, ProjectModel, View} from "./cuba-model";
 import * as ts from "typescript";
+import {LiteralTypeNode} from "typescript";
+import {collectAttributesFromHierarchy} from "../utils";
 
 const VIEW_NAME_TYPE_SUFFIX = 'ViewName';
 const VIEW_NAME_TYPE_PARAMETER = 'V';
@@ -13,7 +15,7 @@ export function createEntityViewTypes(entity: Entity, projectModel: ProjectModel
   const views = findViews(entityName, projectModel);
   return [
     createViewNamesType(className, views),
-    createEntityViewType(className, views)
+    createEntityViewType(className, views, collectAttributesFromHierarchy(entity, projectModel))
   ];
 }
 
@@ -35,14 +37,17 @@ function createViewNamesType(className: string, views: View[]): ts.TypeAliasDecl
 
 
 /**
+ * todo - draw colon on new line
  * e.g.
  *
  * export type EntityView<V extends EntityViewName> = V extends '_minimal' ? Pick<Entity, 'id', 'name'> : never
  *
  * @param className
  * @param views
+ * @param allowedAttrs As of CUBA specific - project model could contains properties from inheritors, we need to
+ * filter out such properties from generated views
  */
-function createEntityViewType(className: string, views: View[]): ts.TypeAliasDeclaration {
+function createEntityViewType(className: string, views: View[], allowedAttrs: EntityAttribute[]): ts.TypeAliasDeclaration {
   const typeNode: ts.TypeNode = views
     .filter(view => view.allProperties.length > 0)
     .reduceRight(
@@ -52,7 +57,7 @@ function createEntityViewType(className: string, views: View[]): ts.TypeAliasDec
           ts.createLiteralTypeNode(
             ts.createLiteral(view.name)
           ),
-          createPickPropertiesType(className, view),
+          createPickPropertiesType(className, view, allowedAttrs),
           typeExpr
         )
       },
@@ -83,17 +88,24 @@ function createEntityViewType(className: string, views: View[]): ts.TypeAliasDec
  *
  * @param className
  * @param view
+ * @param allowedAttrs As of CUBA specific - project model could contains properties from inheritors, we need to
+ * filter out such properties from generated views
  */
-function createPickPropertiesType(className: string, view: View): ts.TypeReferenceNode {
+function createPickPropertiesType(className: string, view: View, allowedAttrs: EntityAttribute[]): ts.TypeReferenceNode {
+
+  const viewTypeNodes: LiteralTypeNode[] = view.allProperties
+    .filter(viewProperty => allowedAttrs.some(attr => attr.name === viewProperty.name))
+    .map(property =>
+      ts.createLiteralTypeNode(
+        ts.createLiteral(property.name)
+      )
+    );
+
   return ts.createTypeReferenceNode(
     'Pick',
     [
       ts.createTypeReferenceNode(className, undefined),
-      ts.createUnionTypeNode(view.allProperties.map(property =>
-        ts.createLiteralTypeNode(
-          ts.createLiteral(property.name)
-        )
-      ))
+      ts.createUnionTypeNode(viewTypeNodes)
     ]
   );
 }
--- packages/front-generator/src/common/utils.ts
+++ packages/front-generator/src/common/utils.ts
@@ -1,4 +1,4 @@
-import {Entity, getEntitiesArray, ProjectModel} from "./model/cuba-model";
+import {Entity, EntityAttribute, getEntitiesArray, ProjectModel} from "./model/cuba-model";
 import * as path from "path";
 import {ProjectEntityInfo} from "./model/entities-generation";
 import {EnumDeclaration} from "typescript";
@@ -69,4 +69,30 @@ export function collectModelContext(projectModel: ProjectModel) {
   entities.forEach(addEntityToMap(entitiesMap));
   baseProjectEntities.forEach(addEntityToMap(entitiesMap, true));
   return {entitiesMap, enumsMap}
+}
+
+//todo add assert in integration test with inheritor attrs (sec$User)
+export function collectAttributesFromHierarchy(entity: Entity, projectModel: ProjectModel): EntityAttribute[] {
+  let attrs: EntityAttribute[] = entity.attributes;
+
+  const allEntities: Partial<Entity>[] = ([] as Partial<Entity>[])
+    .concat(projectModel.entities)
+    .concat(projectModel.baseProjectEntities ? projectModel.baseProjectEntities : []);
+
+  let {parentClassName, parentPackage} = entity;
+
+  while (parentClassName && parentPackage && parentClassName.length > 0 && parentPackage.length > 0) {
+    const parentFqn = `${parentPackage}.${parentClassName}`;
+    const parent = allEntities.find(e => e.fqn === parentFqn);
+    if (parent) {
+      attrs = parent.attributes ? attrs.concat(parent.attributes) : attrs;
+      parentPackage = parent.parentPackage ? parent.parentPackage : '';
+      parentClassName = parent.parentClassName ? parent.parentClassName : '';
+    } else {
+      parentPackage = '';
+      parentClassName = '';
+    }
+  }
+
+  return attrs;
 }
