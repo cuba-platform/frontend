034d61c32eec9a21274bee8303544cd05ff929d1
Vadim Basko <vadim.p.basko@gmail.com>
2019-12-18 17:12:38 +0400

    Form fields are not displayed if roles are not loaded #25

--- packages/cuba-react/src/app/Security.ts
+++ packages/cuba-react/src/app/Security.ts
@@ -1,6 +1,7 @@
 import {action, IObservableArray, observable, ObservableMap} from 'mobx';
 import {CubaApp, EntityAttrPermissionValue, PermissionInfo, RoleInfo, RolesInfo} from '@cuba-platform/rest';
 import {getAttributePermission} from '@cuba-platform/rest/dist-node/security';
+import {matchesVersion} from '@cuba-platform/rest/dist-node/util';
 
 export class Security {
 
@@ -13,6 +14,10 @@ export class Security {
   }
 
   getAttributePermission = (entityName: string, attributeName: string): EntityAttrPermissionValue => {
+
+    // do not check permissions if roles not set (rest api version prev 7.2)
+     if (!this.roles) return 'MODIFY';
+
     const attrFqn = `${entityName}:${attributeName}`;
 
     let perm = this.attrPermissionCache.get(attrFqn);
@@ -24,17 +29,27 @@ export class Security {
   };
 
   @action
-  loadPermissions() {
+  async loadPermissions() {
+
+    await this.cubaREST.refreshApiVersion();
+
     const requestId = ++this.permissionsRequestCount;
-    // todo Will fail on current and previous versions of REST API. Should be changed once cuba-platform/cuba-rest-js#20 is supported
-    this.cubaREST.getRoles()
-      .then(action((rolesInfo: RolesInfo) => {
-        if (requestId === this.permissionsRequestCount) {
-          this.permissions = observable(rolesInfo.permissions);
-          this.roles = observable(rolesInfo.roles);
-          this.attrPermissionCache.clear();
-        }
-      }));
+
+    if (matchesVersion(this.cubaREST.apiVersion, '7.2')) {
+      const rolesInfo: RolesInfo = await this.cubaREST.getRoles();
+      if (requestId === this.permissionsRequestCount) {
+        this.permissions = observable(rolesInfo.permissions);
+        this.roles = observable(rolesInfo.roles);
+        this.attrPermissionCache.clear();
+      }
+
+    } else {
+      const permissions: PermissionInfo[] = await this.cubaREST.getPermissions();
+      if (requestId === this.permissionsRequestCount) {
+        this.permissions = observable(permissions);
+      }
+    }
+
   }
 
 }
