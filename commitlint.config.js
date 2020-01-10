cc2c1d00b971ed9e18be19210a72a64de23a5f71
Vyacheslav Pushkin <v.pushkin@haulmont.com>
2020-01-10 17:37:13 +0400

    chore: add custom rules to commitlint
    the following rules were added:
    - scope shall be in the predefined set of scopes (violation results
    in an error)
    - references should not be empty (violation results in a warning: empty references may
    indicate missing issue number)
    - scope case rule is disabled (scope may have any case)

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,11 @@
-module.exports = {extends: ['@commitlint/config-conventional']}
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'scope-enum': [2, 'always', [
+      'React', 'React Native', 'SDK',
+      'DataTable'
+    ]],
+    'scope-case': [0],
+    'references-empty': [1, 'never']
+  }
+};
