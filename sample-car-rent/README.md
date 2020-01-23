97d39c4811f2164ad983eff23d797d1d25d35240
Vadim Basko <vadim.p.basko@gmail.com>
2020-01-23 08:45:42 +0400

    chore: extends scr model
    add scr$Garage fields and data
    add lookupPicker for Garage on Car editor

--- sample-car-rent/README.md
+++ sample-car-rent/README.md
@@ -10,10 +10,9 @@ manager/2
 
 ## Development
 ### Create Init Script With Data Already Added in App
-* Setup and run Squirrel sql as described [here](https://doc.cuba-platform.com/manual-6.10/db_hsql_connect.html)
-* Run sample-car-rent app
-* Connect to hsql via squirrel used db settings from [context.xml](modules/core/web/META-INF/context.xml)
-* In Squirrel 'Objects' tab find table that should be exported. Click right mouse button on table and run 
-'Scripts' -> 'Create Data Script'
+* Export data from table <TABLE_NAME>
+```bash
+sudo -u postgres pg_dump --table=<TABLE_NAME> --data-only --column-inserts scr
+```
 * Paste generated script to [30.create-db.sql](modules/core/db/init/hsql/30.create-db.sql) 
 * Script will be executed when database will be created next time  
--- sample-car-rent/modules/global/src/com/company/scr/entity/Garage.java
+++ sample-car-rent/modules/global/src/com/company/scr/entity/Garage.java
@@ -1,10 +1,14 @@
 package com.company.scr.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.persistence.Column;
-import com.haulmont.cuba.core.entity.StandardEntity;
 import com.haulmont.chile.core.annotations.NamePattern;
+import com.haulmont.cuba.core.entity.StandardEntity;
+import com.haulmont.cuba.core.entity.annotation.OnDelete;
+import com.haulmont.cuba.core.global.DeletePolicy;
+import com.haulmont.cuba.security.entity.User;
+
+import javax.persistence.*;
+import java.time.LocalTime;
+import java.util.List;
 
 @NamePattern("%s|name")
 @Table(name = "SCR_GARAGE")
@@ -15,8 +19,67 @@ public class Garage extends StandardEntity {
     @Column(name = "NAME", nullable = false)
     protected String name;
 
+    @Column(name = "ADDRESS")
+    protected String address;
+
+    @JoinTable(name = "SCR_GARAGE_USER_LINK",
+            joinColumns = @JoinColumn(name = "GARAGE_ID"),
+            inverseJoinColumns = @JoinColumn(name = "USER_ID"))
+    @OnDelete(DeletePolicy.CASCADE)
+    @ManyToMany
+    protected List<User> personnel;
+
     @Column(name = "CAPACITY")
     protected Integer capacity;
+
+    @Column(name = "VAN_ENTRY")
+    protected Boolean vanEntry;
+
+    @Column(name = "WORKING_HOURS_FROM")
+    protected LocalTime workingHoursFrom;
+
+    @Column(name = "WORKING_HOURS_TO")
+    protected LocalTime workingHoursTo;
+
+    public LocalTime getWorkingHoursTo() {
+        return workingHoursTo;
+    }
+
+    public void setWorkingHoursTo(LocalTime workingHoursTo) {
+        this.workingHoursTo = workingHoursTo;
+    }
+
+    public LocalTime getWorkingHoursFrom() {
+        return workingHoursFrom;
+    }
+
+    public void setWorkingHoursFrom(LocalTime workingHoursFrom) {
+        this.workingHoursFrom = workingHoursFrom;
+    }
+
+    public Boolean getVanEntry() {
+        return vanEntry;
+    }
+
+    public void setVanEntry(Boolean vanEntry) {
+        this.vanEntry = vanEntry;
+    }
+
+    public List<User> getPersonnel() {
+        return personnel;
+    }
+
+    public void setPersonnel(List<User> personnel) {
+        this.personnel = personnel;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
     public void setName(String name) {
         this.name = name;
