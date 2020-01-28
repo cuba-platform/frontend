af69c7d3a8033c265605e1eb7dfd3d57ad9b749e
Vyacheslav Pushkin <v.pushkin@haulmont.com>
2020-01-28 12:37:52 +0400

    refactor(React Native): create a separate file for error codes
    affects: @cuba-platform/front-generator
    
    related to #20

---
+++ packages/front-generator/src/generators/react-native/app/template/rest/errorCodes.ts
@@ -0,0 +1,5 @@
+/**
+ * Error code indicating that authorization grant is not valid (for example, login credentials are incorrect).
+ * Refer to OAuth 2.0 specification at https://tools.ietf.org/html/rfc6749#section-5.2 for more details
+ */
+export const OAUTH2_INVALID_GRANT = 'invalid_grant';
