f4b1c6bc9a05f850e6ddf03ce7f199da25002bb2
Vadim Basko <vadim.p.basko@gmail.com>
2020-02-02 14:26:14 +0400

    test(React): rework i18n message generation test
    affects: @cuba-platform/front-generator
    
    sinonjs used to test full file functionality
    removed export of private function, now it's tested as
    part of public one

--- packages/front-generator/src/generators/react-typescript/common/i18n.ts
+++ packages/front-generator/src/generators/react-typescript/common/i18n.ts
@@ -1,5 +1,17 @@
-import { MemFsEditor } from "yeoman-generator";
+import * as path from 'path';
+import {MemFsEditor} from "yeoman-generator";
 
+/**
+ * Generates and add 'router.className = Class Name' message to i18n en file.
+ * Also add set of en and ru messages passed in parameters ('enJson', 'ruJson') to existed i18n files.
+ * If message already exist in file - it will be overwritten with new value.
+ *
+ * @param fs - yeoman fs
+ * @param className - component class name //todo may be pass className for router as part of enJson object
+ * @param dirShift - directory depth from project root
+ * @param enJson - object with key-value messages to be added in en file
+ * @param ruJson - object with key-value messages to be added in ru file
+ */
 export function writeI18nMessages(
   fs: MemFsEditor,
   className: string,
@@ -7,8 +19,8 @@ export function writeI18nMessages(
   enJson: Record<string, string> = {},
   ruJson: Record<string, string> = {},
 ) {
-  const i18nMessagesPathEn = dirShift + 'i18n/en.json';
-  const i18nMessagesPathRu = dirShift + 'i18n/ru.json';
+  const i18nMessagesPathEn = path.join(dirShift, 'i18n/en.json');
+  const i18nMessagesPathRu = path.join(dirShift, 'i18n/ru.json');
 
   const existingMessagesEn = fs.readJSON(i18nMessagesPathEn);
   const existingMessagesRu = fs.readJSON(i18nMessagesPathRu);
@@ -19,7 +31,8 @@ export function writeI18nMessages(
   fs.writeJSON(i18nMessagesPathRu, ruOut);
 }
 
-export function mergeI18nMessages(
+// todo return null and do not rewrite messages file if there are no changes
+function mergeI18nMessages(
   enExisting: Record<string, string>,
   enTemplate: Record<string, string>,
   ruExisting: Record<string, string>,
