1cb05daa69fc6bff8460b69fbfb0f86d688d45fc
Vadim Basko <vadim.p.basko@gmail.com>
2020-02-02 20:35:23 +0400

    chore(React): improve i18n message generation for component
    affects: @cuba-platform/front-generator
    
    due to component generation do not write in i18n file, if no new keys should be added from this
    component

--- packages/front-generator/src/generators/react-typescript/common/i18n.ts
+++ packages/front-generator/src/generators/react-typescript/common/i18n.ts
@@ -5,7 +5,7 @@ import {capitalizeFirst, splitByCapitalLetter} from "../../../common/utils";
 /**
  * Add set of en and ru component messages to existed i18n files.
  * Also, based on class name, generates and add component menu item caption in en.json file.
- * If any message already exist in file - it will be overwritten with new value.
+ * If any message already exist in file - it will be NOT overwritten with new value.
  *
  * @param fs - yeoman fs
  * @param className - component class name
@@ -23,35 +23,61 @@ export function writeComponentI18nMessages(
   const i18nMessagesPathEn = path.join(dirShift, 'i18n/en.json');
   const i18nMessagesPathRu = path.join(dirShift, 'i18n/ru.json');
 
-  const existingMessagesEn = fs.readJSON(i18nMessagesPathEn);
-  const existingMessagesRu = fs.readJSON(i18nMessagesPathRu);
+  const existingMessagesEn: Record<string, string> | null = fs.readJSON(i18nMessagesPathEn);
+  const existingMessagesRu: Record<string, string> | null = fs.readJSON(i18nMessagesPathRu);
 
   const {enOut, ruOut} = mergeI18nMessages(existingMessagesEn, enJson, existingMessagesRu, ruJson, className);
 
-  fs.writeJSON(i18nMessagesPathEn, enOut);
-  fs.writeJSON(i18nMessagesPathRu, ruOut);
+  // mergeI18nMessages could returns 'null' as out in case we don't have nor new nor changed rows in it
+  enOut && fs.writeJSON(i18nMessagesPathEn, enOut);
+  ruOut && fs.writeJSON(i18nMessagesPathRu, ruOut);
 }
 
-// todo return null and do not rewrite messages file if there are no changes
+/**
+ * Method could returns null for 'en' or 'ru' out if no changes need to be made in file.
+ *
+ * @param enExisting en rows already existed in i18n file
+ * @param enTemplate en rows to be added
+ * @param ruExisting ru rows already existed in i18n file
+ * @param ruTemplate ru rows to be added
+ * @param className menu caption will be generated based on this name and added to en file
+ */
 function mergeI18nMessages(
-  enExisting: Record<string, string>,
+  enExisting: Record<string, string> | null,
   enTemplate: Record<string, string>,
-  ruExisting: Record<string, string>,
+  ruExisting: Record<string, string> | null,
   ruTemplate: Record<string, string>,
   className: string)
-  : { enOut: Record<string, string>, ruOut: Record<string, string> } {
+  : { enOut: Record<string, string> | null, ruOut: Record<string, string> | null } {
 
   const menuCaption = splitByCapitalLetter(capitalizeFirst(className));
-  const enOut = {
+
+  enTemplate = {
     ...enTemplate,
-    [`router.${className}`]: menuCaption,
-    ...enExisting
+    [`router.${className}`]: menuCaption
   };
 
-  const ruOut = {
-    ...ruTemplate,
-    ...ruExisting
-  };
+  const enOut  = hasNewEntries(enTemplate, enExisting)
+    ? {
+    ...enTemplate,
+    ...enExisting
+  } : null;
+
+  const ruOut = hasNewEntries(ruTemplate, ruExisting)
+    ? {
+      ...ruTemplate,
+      ...ruExisting
+    } : null;
 
   return {enOut, ruOut};
 }
+
+function hasNewEntries(newVals: Record<string, string>, oldVals: Record<string, string> | null): boolean {
+  const newKeys = Object.keys(newVals);
+
+  if (newKeys.length === 0) return false;
+  if (!oldVals) return true;
+
+  return !newKeys.every((newK) => Object.keys(oldVals).some((oldK) => oldK === newK));
+}
+
--- packages/front-generator/src/test/generators/react-typrscript/common/i18n.test.ts
+++ packages/front-generator/src/test/generators/react-typrscript/common/i18n.test.ts
@@ -2,38 +2,62 @@ import {writeComponentI18nMessages} from '../../../../generators/react-typescrip
 import {expect, use} from 'chai';
 import * as sinon from 'sinon';
 import * as sinonChai from 'sinon-chai';
-import {MemFsEditor} from "yeoman-generator";
 
 use(sinonChai);
-
-const writeJSON = sinon.fake();
-const readJSON = sinon.fake();
-const fs: MemFsEditor = {readJSON, writeJSON} as any;
 
 const expectEnPath = 'directory/shift/i18n/en.json';
 const expectRuPath = 'directory/shift/i18n/ru.json';
 
+const enExisting = {
+  'key1': 'predefined value 1',
+  'key3': 'predefined value 3',
+  'keyNotInTemplate': 'key not in template'
+};
+
+const ruExisting = {
+  'key1': 'заранее заданное значение 1',
+  'key3': 'заранее заданное значение 3',
+  'key4': 'заранее заданное значение 4',
+  'keyNotInTemplate': 'ключ, отсутствующий в шаблоне'
+};
+
+let fs: any;
+let writeJSON: any;
+
 describe('i18n generation', () => {
+
+  beforeEach(() => {
+    writeJSON = sinon.fake();
+    fs = {writeJSON} as any;
+  });
 
   it('should write i18n messages when message files are empty', () => {
 
+    const readJSON = sinon.stub();
+    fs.readJSON = readJSON;
+
+    readJSON.returns(null);
     writeComponentI18nMessages(fs, "componentClassName", './directory/shift');
 
     expect(readJSON).calledWith(expectRuPath);
     expect(readJSON).calledWith(expectEnPath);
 
-    expect(writeJSON).calledWith(expectRuPath, {});
+    expect(writeJSON).not.calledWith(expectRuPath);
+    expect(writeJSON).calledWith(
+      expectEnPath, {'router.componentClassName': 'Component Class Name'});
+
+    readJSON.returns({});
+    writeComponentI18nMessages(fs, "componentClassName", './directory/shift');
+
+    expect(readJSON).calledWith(expectRuPath);
+    expect(readJSON).calledWith(expectEnPath);
+
+    expect(writeJSON).not.calledWith(expectRuPath);
     expect(writeJSON).calledWith(
       expectEnPath, {'router.componentClassName': 'Component Class Name'});
   });
 
   it('should merge and write i18n messages', function () {
-
-    const enExisting = {
-      'key1': 'predefined value 1',
-      'key3': 'predefined value 3',
-      'keyNotInTemplate': 'key not in template'
-    };
 
     const enTemplate = {
       'key1': 'template value 1',
@@ -41,13 +65,6 @@ describe('i18n generation', () => {
       'key3': 'template value 3',
       'key4': 'template value 4',
       'key5': 'template value 5',
-    };
-
-    const ruExisting = {
-      'key1': 'заранее заданное значение 1',
-      'key3': 'заранее заданное значение 3',
-      'key4': 'заранее заданное значение 4',
-      'keyNotInTemplate': 'ключ, отсутствующий в шаблоне'
     };
 
     const ruTemplate = {
@@ -86,6 +103,36 @@ describe('i18n generation', () => {
 
     expect(writeJSON).calledWith(expectEnPath, enExpected);
     expect(writeJSON).calledWith(expectRuPath, ruExpected);
+  });
+
+  it('should not call write method, if no rows are added', function () {
+
+    const readJSON = sinon.stub();
+    fs.readJSON = readJSON;
+
+    readJSON.withArgs(expectRuPath).returns(ruExisting);
+    // case where all keys, include 'router.*' already exist in file and have the same values
+    readJSON.withArgs(expectEnPath).returns({'router.testClass':'Test Class', ...enExisting});
+
+    writeComponentI18nMessages(fs, 'testClass', './directory/shift');
+    expect(writeJSON).not.calledWith(expectRuPath);
+    expect(writeJSON).not.calledWith(expectEnPath);
+
+    let enJson = { 'key1': 'predefined value 1'};
+    writeComponentI18nMessages(fs, 'testClass', './directory/shift', enJson);
+    expect(writeJSON).not.calledWith(expectRuPath);
+    expect(writeJSON).not.calledWith(expectEnPath);
+
+    writeComponentI18nMessages(fs, 'testClass', './directory/shift', enExisting, ruExisting);
+    expect(writeJSON).not.calledWith(expectRuPath);
+    expect(writeJSON).not.calledWith(expectEnPath);
+
+    readJSON.withArgs(expectEnPath).returns({'router.testClass':'Test Class', ...enExisting});
+
+    enJson = { 'key1': 'new value 1'};
+    writeComponentI18nMessages(fs, 'testClass', './directory/shift', enJson);
+    expect(writeJSON).not.calledWith(expectRuPath);
+    expect(writeJSON).not.calledWith(expectRuPath);
   });
 
 });
