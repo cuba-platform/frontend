3aa35cf81b227aec27133ca784ce74147b15e7e0
Vadim Basko <vadim.p.basko@gmail.com>
2020-02-02 14:26:14 +0400

    test(React): rework i18n message generation test
    affects: @cuba-platform/front-generator
    
    sinonjs used to test full file functionality
    removed export of private function, now it's tested as
    part of public one

--- packages/front-generator/src/test/generators/react-typrscript/common/i18n.test.ts
+++ packages/front-generator/src/test/generators/react-typrscript/common/i18n.test.ts
@@ -1,59 +1,92 @@
-import {mergeI18nMessages} from '../../../../generators/react-typescript/common/i18n';
-import { expect } from 'chai';
+import {writeI18nMessages} from '../../../../generators/react-typescript/common/i18n';
+import {expect, use} from 'chai';
+import * as sinon from 'sinon';
+import * as sinonChai from 'sinon-chai';
+import {MemFsEditor} from "yeoman-generator";
 
-describe('react generator helpers', () => {
-  const enExisting = {
-    'key1': 'predefined value 1',
-    'key3': 'predefined value 3',
-    'keyNotInTemplate': 'key not in template'
-  };
+use(sinonChai);
 
-  const enTemplate = {
-    'key1': 'template value 1',
-    'key2': 'template value 2',
-    'key3': 'template value 3',
-    'key4': 'template value 4',
-    'key5': 'template value 5',
-  };
+const writeJSON = sinon.fake();
+const readJSON = sinon.fake();
+const fs: MemFsEditor = {readJSON, writeJSON} as any;
 
-  const ruExisting = {
-    'key1': 'заранее заданное значение 1',
-    'key3': 'заранее заданное значение 3',
-    'key4': 'заранее заданное значение 4',
-    'keyNotInTemplate': 'ключ, отсутствующий в шаблоне'
-  };
+const expectEnPath = 'directory/shift/i18n/en.json';
+const expectRuPath = 'directory/shift/i18n/ru.json';
 
-  const ruTemplate = {
-    'key1': 'значение из шаблона 1',
-    'key2': 'значение из шаблона 2',
-    'key3': 'значение из шаблона 3',
-    'key4': 'значение из шаблона 4',
-    'key5': 'значение из шаблона 5',
-  };
+describe('i18n generation', () => {
 
-  const enExpected = {
-    'key1': 'predefined value 1',
-    'key2': 'template value 2',
-    'key3': 'predefined value 3',
-    'key4': 'template value 4',
-    'key5': 'template value 5',
-    'router.testClass': 'testClass',
-    'keyNotInTemplate': 'key not in template'
-  };
+  it('should write i18n messages when message files are empty', () => {
 
-  const ruExpected = {
-    'key1': 'заранее заданное значение 1',
-    'key2': 'значение из шаблона 2',
-    'key3': 'заранее заданное значение 3',
-    'key4': 'заранее заданное значение 4',
-    'key5': 'значение из шаблона 5',
-    'keyNotInTemplate': 'ключ, отсутствующий в шаблоне'
-  };
+    writeI18nMessages(fs, "componentClassName", './directory/shift');
 
-  it('should add i18n keys without overwriting', () => {
-    const {enOut, ruOut} = mergeI18nMessages(enExisting, enTemplate, ruExisting, ruTemplate, 'testClass');
+    expect(readJSON).calledWith(expectRuPath);
+    expect(readJSON).calledWith(expectEnPath);
 
-    expect(enOut).to.deep.equal(enExpected);
-    expect(ruOut).to.deep.equal(ruExpected);
+    expect(writeJSON).calledWith(expectRuPath, {});
+    expect(writeJSON).calledWith(
+      expectEnPath, {'router.componentClassName': 'componentClassName'});
   });
+
+  it('should merge and write i18n messages', function () {
+
+    const enExisting = {
+      'key1': 'predefined value 1',
+      'key3': 'predefined value 3',
+      'keyNotInTemplate': 'key not in template'
+    };
+
+    const enTemplate = {
+      'key1': 'template value 1',
+      'key2': 'template value 2',
+      'key3': 'template value 3',
+      'key4': 'template value 4',
+      'key5': 'template value 5',
+    };
+
+    const ruExisting = {
+      'key1': 'заранее заданное значение 1',
+      'key3': 'заранее заданное значение 3',
+      'key4': 'заранее заданное значение 4',
+      'keyNotInTemplate': 'ключ, отсутствующий в шаблоне'
+    };
+
+    const ruTemplate = {
+      'key1': 'значение из шаблона 1',
+      'key2': 'значение из шаблона 2',
+      'key3': 'значение из шаблона 3',
+      'key4': 'значение из шаблона 4',
+      'key5': 'значение из шаблона 5',
+    };
+
+    const enExpected = {
+      'key1': 'predefined value 1',
+      'key2': 'template value 2',
+      'key3': 'predefined value 3',
+      'key4': 'template value 4',
+      'key5': 'template value 5',
+      'router.testClass': 'testClass',
+      'keyNotInTemplate': 'key not in template'
+    };
+
+    const ruExpected = {
+      'key1': 'заранее заданное значение 1',
+      'key2': 'значение из шаблона 2',
+      'key3': 'заранее заданное значение 3',
+      'key4': 'заранее заданное значение 4',
+      'key5': 'значение из шаблона 5',
+      'keyNotInTemplate': 'ключ, отсутствующий в шаблоне'
+    };
+
+    const readJSON = sinon.stub();
+    readJSON.withArgs(expectEnPath).returns(enExisting);
+    readJSON.withArgs(expectRuPath).returns(ruExisting);
+    fs.readJSON = readJSON;
+
+    writeI18nMessages(fs, 'testClass', './directory/shift', enTemplate, ruTemplate);
+
+    expect(writeJSON).calledWith(expectEnPath, enExpected);
+    expect(writeJSON).calledWith(expectRuPath, ruExpected);
+  });
+
 });
+
