449ce80d11bcf5986874bc8b1ac38b77e3ef2548
Vadim Basko <vadim.p.basko@gmail.com>
2020-02-02 15:32:48 +0400

    feat(React): component class name generation improvement
    affects: @cuba-platform/front-generator
    
    generates valid name, if spaces exist in initial element name
    symbols not allowed for TS class name
    now removed before class name generation
    
    relates #70

--- packages/front-generator/src/test/utils.test.ts
+++ packages/front-generator/src/test/utils.test.ts
@@ -2,17 +2,21 @@ import {convertToUnixPath, elementNameToClass, fqnToName} from "../common/utils"
 import * as assert from "assert";
 
 
-describe('utils', function () {
-  it("elementNameToClass", function () {
+describe('utils', () => {
+  it("elementNameToClass", () => {
     assert.strictEqual(elementNameToClass('my-custom-el'), 'MyCustomEl');
     assert.strictEqual(elementNameToClass('x-f'), 'XF');
+    assert.strictEqual(elementNameToClass('myCustomEl'), 'MyCustomEl');
+    assert.strictEqual(elementNameToClass('my custom El'), 'MyCustomEl');
+    assert.strictEqual(elementNameToClass('  my   veryCustom  El   '), 'MyVeryCustomEl');
+    assert.strictEqual(elementNameToClass('~!@#%^&*()_+the Cla$$'), '_theCla$$');
   });
 
-  it(convertToUnixPath.name, function () {
+  it(convertToUnixPath.name, () => {
     assert.strictEqual(convertToUnixPath('.\\some\\path'), './some/path');
   });
 
-  it('should convert fqn name to valid TS class name ', function () {
+  it('should convert fqn name to valid TS class name ', () => {
     assert.strictEqual(fqnToName('com.company.mpg.entity.CarType'), 'com_company_mpg_entity_CarType');
     assert.strictEqual(fqnToName(''), '');
   });
