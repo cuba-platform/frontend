c9a6d04c3fa78402d9e002d3fd6d52788990aab0
Vyacheslav Pushkin <v.pushkin@haulmont.com>
2020-02-03 18:39:32 +0400

    feat: allow overriding data transfer/display formats for dates
    affects: @cuba-platform/react-core

--- packages/cuba-react-core/src/app/CubaAppProvider.tsx
+++ packages/cuba-react-core/src/app/CubaAppProvider.tsx
@@ -1,14 +1,16 @@
-import {CubaApp} from "@cuba-platform/rest";
+import {CubaApp, PropertyType} from "@cuba-platform/rest";
 import * as React from "react";
 import {MainStore} from "./MainStore";
 import {Provider} from "mobx-react";
 
 let cubaAppContext: React.Context<CubaAppContextValue>;
+let cubaAppConfig: CubaAppConfig | undefined;
 let globalCubaREST: CubaApp;
 let mainStore: MainStore;
 
 export interface CubaAppContextValue {
   cubaREST?: CubaApp;
+  config?: CubaAppConfig;
 }
 
 export function getCubaREST(): CubaApp | undefined {
@@ -19,17 +21,28 @@ export function getMainStore(): MainStore {
   return mainStore;
 }
 
+export function getCubaAppConfig(): CubaAppConfig | undefined {
+  return cubaAppConfig;
+}
+
+export interface CubaAppConfig {
+  dataTransferFormats?: Partial<Record<PropertyType, string>>;
+  displayFormats?: Partial<Record<PropertyType, string>>;
+}
+
 export interface CubaAppProviderProps {
   cubaREST: CubaApp;
   children: React.ReactNode | React.ReactNode[] | null;
   retrieveRestApiToken?: () => Promise<string|undefined>;
+  config?: CubaAppConfig;
 }
 
 export const CubaAppProvider: React.FC<CubaAppProviderProps> = (
   {
     cubaREST,
     children,
-    retrieveRestApiToken = () => Promise.resolve(undefined)
+    retrieveRestApiToken = () => Promise.resolve(undefined),
+    config
   }
 ) => {
   const CubaAppContext = getContext();
@@ -37,6 +50,7 @@ export const CubaAppProvider: React.FC<CubaAppProviderProps> = (
     <CubaAppContext.Consumer>
       {(context = {}) => {
         if (cubaREST && context.cubaREST !== cubaREST) {
+          cubaAppConfig = config;
           globalCubaREST = cubaREST;
           mainStore = new MainStore(cubaREST);
           retrieveRestApiToken().then((restApiToken) => {
