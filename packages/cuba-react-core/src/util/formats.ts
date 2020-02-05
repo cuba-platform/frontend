b7fe886aed9df2dace1fd582dec5e3b5e52f381d
Vyacheslav Pushkin <v.pushkin@haulmont.com>
2020-02-05 15:27:19 +0400

    style: change variable naming
    affects: @cuba-platform/react-core

--- packages/cuba-react-core/src/util/formats.ts
+++ packages/cuba-react-core/src/util/formats.ts
@@ -1,37 +1,37 @@
 import {PropertyType} from '@cuba-platform/rest';
 import {getCubaAppConfig} from '..';
 
-export const defaultDateFormat = 'YYYY-MM-DD';
-export const defaultTimeFormat = 'HH:mm:ss';
+export const DEFAULT_DATE_FORMAT = 'YYYY-MM-DD';
+export const DEFAULT_TIME_FORMAT = 'HH:mm:ss';
 
-export const defaultDateTimeDataTransferFormat = 'YYYY-MM-DD HH:mm:ss.SSS';
-export const defaultDateTimeDisplayFormat = 'YYYY-MM-DD HH:mm:ss';
-export const offsetDateTimeDataTransferFormat = 'YYYY-MM-DD HH:mm:ss.SSS ZZ';
-export const offsetDateTimeDisplayFormat = 'YYYY-MM-DD HH:mm:ss';
+export const DEFAULT_DATE_TIME_DATA_TRANSFER_FORMAT = 'YYYY-MM-DD HH:mm:ss.SSS';
+export const DEFAULT_DATE_TIME_DISPLAY_FORMAT = 'YYYY-MM-DD HH:mm:ss';
+export const OFFSET_DATE_TIME_DATE_TRANSFER_FORMAT = 'YYYY-MM-DD HH:mm:ss.SSS ZZ';
+export const OFFSET_DATE_TIME_DISPLAY_FORMAT = 'YYYY-MM-DD HH:mm:ss';
 
-export const offsetTimeDataTransferFormat = 'HH:mm:ss ZZ';
-export const offsetTimeDisplayFormat = 'HH:mm:ss';
+export const OFFSET_TIME_DATA_TRANSFER_FORMAT = 'HH:mm:ss ZZ';
+export const OFFSET_TIME_DISPLAY_FORMAT = 'HH:mm:ss';
 
 export const defaultDataTransferFormats: Partial<Record<PropertyType, string>> = {
-  date: defaultDateFormat,
-  time: defaultTimeFormat,
-  dateTime: defaultDateTimeDataTransferFormat,
-  localDate: defaultDateFormat,
-  localTime: defaultTimeFormat,
-  localDateTime: defaultDateTimeDataTransferFormat,
-  offsetDateTime: offsetDateTimeDataTransferFormat,
-  offsetTime: offsetTimeDataTransferFormat
+  date: DEFAULT_DATE_FORMAT,
+  time: DEFAULT_TIME_FORMAT,
+  dateTime: DEFAULT_DATE_TIME_DATA_TRANSFER_FORMAT,
+  localDate: DEFAULT_DATE_FORMAT,
+  localTime: DEFAULT_TIME_FORMAT,
+  localDateTime: DEFAULT_DATE_TIME_DATA_TRANSFER_FORMAT,
+  offsetDateTime: OFFSET_DATE_TIME_DATE_TRANSFER_FORMAT,
+  offsetTime: OFFSET_TIME_DATA_TRANSFER_FORMAT
 };
 
 export const defaultDisplayFormats: Partial<Record<PropertyType, string>> = {
-  date: defaultDateFormat,
-  time: defaultTimeFormat,
-  dateTime: defaultDateTimeDisplayFormat,
-  localDate: defaultDateFormat,
-  localTime: defaultTimeFormat,
-  localDateTime: defaultDateTimeDisplayFormat,
-  offsetDateTime: offsetDateTimeDisplayFormat,
-  offsetTime: offsetTimeDisplayFormat
+  date: DEFAULT_DATE_FORMAT,
+  time: DEFAULT_TIME_FORMAT,
+  dateTime: DEFAULT_DATE_TIME_DISPLAY_FORMAT,
+  localDate: DEFAULT_DATE_FORMAT,
+  localTime: DEFAULT_TIME_FORMAT,
+  localDateTime: DEFAULT_DATE_TIME_DISPLAY_FORMAT,
+  offsetDateTime: OFFSET_DATE_TIME_DISPLAY_FORMAT,
+  offsetTime: OFFSET_TIME_DISPLAY_FORMAT
 };
 
 export function getDataTransferFormat(type: PropertyType): string | undefined {
