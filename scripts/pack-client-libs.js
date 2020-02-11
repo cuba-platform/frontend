fbe7faedecd1a6691e42669999fbfb6fed92905d
Vadim Basko <vadim.p.basko@gmail.com>
2020-02-11 09:30:06 +0400

    chore: improve pack-client-libs scripts
    scrit doesn't work for single lib, or if there are no libs provided
    front-generator package name
    fix

--- scripts/pack-client-libs.js
+++ scripts/pack-client-libs.js
@@ -1,13 +1,27 @@
 const {runCmdSync} = require('./common');
 
 const packClientLibs = (prepublishLibs, packLibs) => {
-    const prepublishScope = prepublishLibs.join(',');
-    const packScope = packLibs.join(',');
 
-    let prepublishCmd = `lerna run prepublishOnly --scope '{${prepublishScope}}'`;
-    let execCmd = `lerna exec --scope '{${packScope}}' 'npm pack -q'`;
+  const prepublishScope = composeScope(prepublishLibs);
+  const packScope = composeScope(packLibs);
 
-    runCmdSync(`${prepublishCmd} && ${execCmd}`);
+  const prepublishCmd = prepublishScope && `lerna run prepublishOnly ${prepublishScope}`;
+  const packCmd = packScope && `lerna exec ${packScope} 'npm pack -q'`;
+
+  let cmd = prepublishCmd ? prepublishCmd : '';
+  if (cmd.length > 0 && packCmd) cmd += ' && ';
+  cmd = packCmd ? cmd + packCmd : cmd;
+
+  console.log(`${cmd}`);
+  runCmdSync(`${cmd}`);
+};
+
+const composeScope = (libs) => {
+  if (!libs || libs.length === 0) return null;
+
+  return libs.length > 1
+    ? `--scope '{${libs.join(',')}}'`
+    : `--scope '${libs[0]}'`; //curly brackets not work for array with single element
 };
 
 module.exports = packClientLibs;
--- scripts/pack-react-native-client-libs.js
+++ scripts/pack-react-native-client-libs.js
@@ -2,7 +2,7 @@ const packClientLibs = require('./pack-client-libs');
 
 packClientLibs(
     [
-        '@cuba-platform/frontend-generator',
+        '@cuba-platform/front-generator',
         '@cuba-platform/rest',
         '@cuba-platform/react-core'
     ],
