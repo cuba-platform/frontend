7d9b94be1aaff1058e8c79eee5df6095b88193eb
Vyacheslav Pushkin <v.pushkin@haulmont.com>
2020-03-31 19:31:59 +0400

    chore: update dependencies (semver-major)
    affects: @cuba-platform/react-core, @cuba-platform/react-ui, @cuba-platform/front-generator
    
    related to #162

--- scripts/run-unit-tests.js
+++
@@ -1,4 +0,0 @@
-const {runCmdSync} = require('./common');
-
-runCmdSync('lerna run test --scope @cuba-platform/react-ui ' +
-    '&& lerna run coverage --scope {@cuba-platform/rest,@cuba-platform/front-generator}');
