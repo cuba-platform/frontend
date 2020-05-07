0bf878798bd173b5ed3ae7c66bfbd78241353dd7
Vyacheslav Pushkin <v.pushkin@haulmont.com>
2020-05-07 18:53:32 +0400

    fix: relax UUID validation rules
    affects: @cuba-platform/react-ui
    
    Related to #197.

--- packages/cuba-react-ui/src/ui/form/UuidInput.tsx
+++ packages/cuba-react-ui/src/ui/form/UuidInput.tsx
@@ -3,13 +3,22 @@ import React, {forwardRef, Ref} from "react";
 import {Input} from 'antd';
 import {InputProps} from 'antd/es/input';
 
+// More strict validation would be:
+//
+// <InputWithMask mask='xxxxxxxx-xxxx-Mxxx-Nxxx-xxxxxxxxxxxx'
+//                formatChars={{
+//                  'x': '[0-9a-fA-F]',
+//                  'M': '[0-5]', // UUID version
+//                  'N': '[089ab]', // UUID variant
+//                }}
+//
+// However, we are using relaxed validation rules as Generic UI allows to create UUIDs with invalid
+// versions and variants (https://github.com/cuba-platform/cuba/issues/2867)
 export const UuidInput = forwardRef((props: InputProps, ref: Ref<Input>) => {
   return (
-    <InputWithMask mask='xxxxxxxx-xxxx-Mxxx-Nxxx-xxxxxxxxxxxx'
+    <InputWithMask mask='xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'
                    formatChars={{
-                     'x': '[0-9a-fA-F]',
-                     'M': '[0-5]', // UUID version
-                     'N': '[089ab]', // UUID variant
+                     'x': '[0-9a-fA-F]'
                    }}
                    ref={ref}
                    {...props}
