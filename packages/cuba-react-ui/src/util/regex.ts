584a8df7ec205ca27a4fd1901e9ac79c7db2785b
Vyacheslav Pushkin <v.pushkin@haulmont.com>
2020-05-07 18:53:32 +0400

    fix: relax UUID validation rules
    affects: @cuba-platform/react-ui
    
    Related to #197.

--- packages/cuba-react-ui/src/util/regex.ts
+++ packages/cuba-react-ui/src/util/regex.ts
@@ -1,4 +1,5 @@
-/**
- * Matches UUID v1-v5. Matches NIL UUID.
- */
-export const uuidPattern = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+// More strict validation would be:
+// export const uuidPattern = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+// However, we are using relaxed validation rules as Generic UI allows to create UUIDs with invalid
+// versions and variants (https://github.com/cuba-platform/cuba/issues/2867)
+export const uuidPattern = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
