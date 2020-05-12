5a2649eb9aadd8d7070b966f29ca56934d9035b4
Vyacheslav Pushkin <v.pushkin@haulmont.com>
2020-05-12 18:33:35 +0400

    chore: allow using snyk wizard
    affects: @cuba-platform/react-core, @cuba-platform/react-ui, @cuba-platform/rest,
    @cuba-platform/front-generator
    
    ticket #200

--- scripts/common.js
+++ scripts/common.js
@@ -8,3 +8,12 @@ exports.runCmdSync = (command, cwd = process.cwd()) => {
         process.exit(errorCode);
     }
 };
+
+const highlightInfo = '\x1b[34m%s\x1b[0m';
+const highlightSuccess = '\x1b[32m%s\x1b[0m';
+const highlightError = '\x1b[31m%s\x1b[0m';
+exports.log = {
+  info: (message) => console.log(highlightInfo, message),
+  success: (message) => console.log(highlightSuccess, message),
+  error: (message) => console.log(highlightError, message),
+};
