d0e119aecf0c823cad4157d9c21ffe9272959c7c
Vyacheslav Pushkin <v.pushkin@haulmont.com>
2020-05-18 16:43:28 +0400

    chore: change commit header max length to 100

--- commitlint.config.js
+++ commitlint.config.js
@@ -10,6 +10,7 @@ module.exports = {
     'type-enum': [2, 'always', [
       'feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'chore', 'revert', 'WIP'
     ]],
-    'type-case': [0]
+    'type-case': [0],
+    'header-max-length': [2, 'always', 100]
   }
 };
