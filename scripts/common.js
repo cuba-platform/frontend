e6a285e9d45e46f55f9266ece8acf909a705e90f
Vadim Basko <vadim.p.basko@gmail.com>
2020-05-23 00:00:59 +0400

    chore: react scr generation scripts - minor improvements

--- scripts/common.js
+++ scripts/common.js
@@ -1,12 +1,13 @@
 const execSync = require('child_process').execSync;
 
 exports.runCmdSync = (command, cwd = process.cwd()) => {
-    try {
-        execSync(command, {stdio: 'inherit', cwd: cwd});
-    } catch (err) {
-        const errorCode = Number.isFinite(parseInt(err.status)) ? parseInt(err.status) : 1;
-        process.exit(errorCode);
-    }
+  try {
+    console.log(command);
+    execSync(command, {stdio: 'inherit', cwd: cwd});
+  } catch (err) {
+    const errorCode = Number.isFinite(parseInt(err.status)) ? parseInt(err.status) : 1;
+    process.exit(errorCode);
+  }
 };
 
 const highlightInfo = '\x1b[34m%s\x1b[0m';
