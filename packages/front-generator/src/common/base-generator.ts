b0ed6336cab4f3e7211619a9a62729385e5b86ac
Vadim Basko <vadim.p.basko@gmail.com>
2020-06-05 15:13:50 +0400

    fix(Front Generator): answers validation for question type INTEGER
    relates: #244
    
    affects: @cuba-platform/front-generator

--- packages/front-generator/src/common/base-generator.ts
+++ packages/front-generator/src/common/base-generator.ts
@@ -166,14 +166,21 @@ export function readProjectModel(modelFilePath: string): ProjectModel {
   return JSON.parse(fs.readFileSync(modelFilePath, "utf8"));
 }
 
-export function refineAnswers<T>(projectModel: ProjectModel, props: StudioTemplateProperty[], answers: any): T {
+// TODO fix answers type
+export function refineAnswers<T>(projectModel: ProjectModel, generatorParams: StudioTemplateProperty[], answers: any): T {
   const refinedAnswers: { [key: string]: any } = {};
+
+  if (answers == null) return refinedAnswers as T;
+
   Object.keys(answers).forEach((key: string) => {
-    const prop = props.find(p => p.code === key);
+    const prop = generatorParams.find(p => p.code === key);
+
+    // leave answer as is if it is not exist in props
     if (prop == null) {
       refinedAnswers[key] = answers[key];
       return;
     }
+
     switch (prop.propertyType) {
       case StudioTemplatePropertyType.ENTITY:
         refinedAnswers[key] = findEntity(projectModel, (answers[key] as EntityInfo));
@@ -186,6 +193,11 @@ export function refineAnswers<T>(projectModel: ProjectModel, props: StudioTempla
         return;
       case StudioTemplatePropertyType.REST_SERVICE_METHOD:
         refinedAnswers[key] = findServiceMethod(projectModel, (answers[key] as RestServiceMethodInfo));
+        return;
+      case StudioTemplatePropertyType.INTEGER:
+        const value = answers[key];
+        if (!Number.isInteger(value)) throw new Error(`Question with code '${key}' has INTEGER type and can't contains '${value}' as answer`);
+        refinedAnswers[key] = value;
         return;
       default:
         refinedAnswers[key] = answers[key];
