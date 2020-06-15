6287b016823263cb35cf1a063eb480245ea8fb9b
Vyacheslav Pushkin <v.pushkin@haulmont.com>
2020-06-15 16:12:45 +0400

    chore: check built clients with Dependabot
    related to #247

---
+++ travis/push-client-builds.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+REPO_URL=https://${GH_TOKEN}@github.com/cuba-labs/frontend-scr-builds.git
+BRANCH=master
+
+echo "Cloning frontend-scr-builds"
+git clone $REPO_URL --branch=$BRANCH > /dev/null 2>&1
+cd frontend-scr-builds || exit
+git remote set-url origin $REPO_URL > /dev/null 2>&1
+
+echo "Copying the built clients"
+cp -r ../react-client-scr .
+cp -r ../react-native-client-scr .
+
+echo "Committing"
+git config user.name "Travis CI"
+git config user.email "<>"
+git add -A
+git commit -m "Build $TRAVIS_BUILD_NUMBER"
+
+echo "Pushing"
+git push --quiet --set-upstream origin $BRANCH
