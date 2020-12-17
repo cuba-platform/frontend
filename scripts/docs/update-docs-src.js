0fe987a4bb894487b24314d467b5123a9ad8709c
Vyacheslav Pushkin <v.pushkin@haulmont.com>
2020-12-17 15:35:20 +0400

    docs: fix API docs generation

--- scripts/docs/update-docs-src.js
+++ scripts/docs/update-docs-src.js
@@ -7,9 +7,9 @@ const highlight = '\x1b[34m%s\x1b[0m';
 const success = '\x1b[32m%s\x1b[0m';
 
 console.log(highlight, 'Generating API Reference documentation from TSDoc comments...');
-rimraf.sync('docs-src/api-reference/cuba-rest-js');
-rimraf.sync('docs-src/api-reference/cuba-react-core');
-rimraf.sync('docs-src/api-reference/cuba-react-ui');
+rimraf.sync('docs-src/api-reference/jmix-rest');
+rimraf.sync('docs-src/api-reference/jmix-react-core');
+rimraf.sync('docs-src/api-reference/jmix-react-ui');
 runCmdSync('lerna run prepublishOnly --scope @haulmont/jmix-rest --scope @haulmont/jmix-react-core --scope @haulmont/jmix-react-ui}');
 runCmdSync('lerna run generate-docs');
 
